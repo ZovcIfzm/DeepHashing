c2e37de20f68b1939bd01143290ffb62a77d86f2
nrocketmann <nameerhirschkind@gmail.com>
2020-11-25 23:11:33 -0800

    reformat

--- NetworkTF.py
+++ NetworkTF.py
@@ -4,7 +4,6 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.models import Model
 from sklearn.decomposition import PCA
 
-
 """
 USAGE:
 This file is meant to be used as a library/module which is imported into other programs in order to test the algorithm.
@@ -13,102 +12,103 @@ The commented out code at the bottom just tests the functions above on dummy dat
 
 """
 
-#There are M+1 layers in the network
-#activation for every layer is tanh, layers are just dense
-#DATA IS ALWAYS ASSUMED TO BE IN ROW VECTORS FOR THIS WHOLE PROGRAM
 
-#W for the first layer is supposed to be initialized to the first [layer_1_size] eigenvectors of the covariance matrix
-#this function just finds those eigenvectors where n_dims is the number to get
+# There are M+1 layers in the network
+# activation for every layer is tanh, layers are just dense
+# DATA IS ALWAYS ASSUMED TO BE IN ROW VECTORS FOR THIS WHOLE PROGRAM
+
+# W for the first layer is supposed to be initialized to the first [layer_1_size] eigenvectors of the covariance matrix
+# this function just finds those eigenvectors where n_dims is the number to get
 def initialize_W(data, n_dims):
     pca = PCA(n_dims).fit(data)
     return pca.components_.T
 
 
-
 class DeepHash(Model):
     def __init__(self, layer_sizes, l2_, l3_, l1_, initial_W):
-        super(DeepHash,self).__init__()
+        super(DeepHash, self).__init__()
         self.layer_sizes = layer_sizes
         self.l1 = l1_
         self.l2 = l2_
         self.l3 = l3_
         self.initial_W = initial_W
 
-    def custom_reg(self,W):
-        #NOTE THAT WE DO THE REVERSE OF THE PAPER WITH OUR TRANSPOSE HERE, SINCE TENSORFLOW STORES WEIGHT MATRICES TRANSPOSED
-        #W is [this_layer_nodes, next_layer_nodes] in shape
+    def custom_reg(self, W):
+        # NOTE THAT WE DO THE REVERSE OF THE PAPER WITH OUR TRANSPOSE HERE, SINCE TENSORFLOW STORES WEIGHT MATRICES TRANSPOSED
+        # W is [this_layer_nodes, next_layer_nodes] in shape
         part1 = self.l2 * tf.square(tf.norm(tf.matmul(tf.transpose(W), W) - tf.eye(W.shape[1])))
-        part2 = self.l3/2 * tf.square(tf.norm(W))
-        return part1+part2
+        part2 = self.l3 / 2 * tf.square(tf.norm(W))
+        return part1 + part2
 
     def custom_bias_reg(self, b):
-        return self.l3/2 * tf.square(tf.norm(b))
+        return self.l3 / 2 * tf.square(tf.norm(b))
 
     def custom_W_init(self, shape, dtype=None):
         return self.initial_W
 
     def build(self, input_shape):
         self.fc_layers = []
-        self.fc_layers.append(Dense(self.layer_sizes[0],activation='tanh',input_shape=input_shape,
+        self.fc_layers.append(Dense(self.layer_sizes[0], activation='tanh', input_shape=input_shape,
                                     kernel_regularizer=self.custom_reg, bias_regularizer=self.custom_bias_reg,
-                                    bias_initializer = 'ones', kernel_initializer=self.custom_W_init))
+                                    bias_initializer='ones', kernel_initializer=self.custom_W_init))
         for layer in self.layer_sizes[1:]:
-            self.fc_layers.append(Dense(layer,activation='tanh',kernel_regularizer=self.custom_reg,
+            self.fc_layers.append(Dense(layer, activation='tanh', kernel_regularizer=self.custom_reg,
                                         bias_regularizer=self.custom_bias_reg, bias_initializer='ones',
                                         kernel_initializer='identity'))
 
-    def call(self,inputs):
+    def call(self, inputs):
         h = inputs
         for layer in self.fc_layers:
             h = layer(h)
         quantized = tf.math.sign(h)
-        self.add_loss(.5 * tf.square(tf.norm(quantized-h))) #both are row vectors, so it's fine
-        #NOTE WE PUT THE TRANSPOSE FIRST BECAUSE WE HAVE ROW VECTORS, NOT COLUMN VECTORS HERE
-        #Trace is not affected by transposing which is why we don't need another transpose on the whole thing
+        self.add_loss(.5 * tf.square(tf.norm(quantized - h)))  # both are row vectors, so it's fine
+        # NOTE WE PUT THE TRANSPOSE FIRST BECAUSE WE HAVE ROW VECTORS, NOT COLUMN VECTORS HERE
+        # Trace is not affected by transposing which is why we don't need another transpose on the whole thing
 
-        N = tf.cast(tf.shape(inputs)[0],tf.float32)
-        self.add_loss(self.l1/(2*N) * tf.linalg.trace(tf.matmul(tf.transpose(h),h)))
+        N = tf.cast(tf.shape(inputs)[0], tf.float32)
+        self.add_loss(self.l1 / (2 * N) * tf.linalg.trace(tf.matmul(tf.transpose(h), h)))
         return h, quantized
 
 
-#This trains a DeepHash model for the specified number of epochs given an optimizer object and training data
-def train_unsupervised(model, epochs,data, optimizer):
+# This trains a DeepHash model for the specified number of epochs given an optimizer object and training data
+def train_unsupervised(model, epochs, data, optimizer):
     for i in range(epochs):
         with tf.GradientTape() as tape:
             out = model(data)
             loss = tf.reduce_sum(model.losses)
-            grad = tape.gradient(loss,model.trainable_variables)
-            optimizer.apply_gradients(zip(grad,model.trainable_variables))
+            grad = tape.gradient(loss, model.trainable_variables)
+            optimizer.apply_gradients(zip(grad, model.trainable_variables))
             print(loss)
 
-#Here pos_pairs and neg_pairs are assumed to be of shape [num_pairs, 2, data_dimension]
-#alpha is the coefficient for the supervised loss
+
+# Here pos_pairs and neg_pairs are assumed to be of shape [num_pairs, 2, data_dimension]
+# alpha is the coefficient for the supervised loss
 def train_supervised(model, epochs, pos_pairs, neg_pairs, data, optimizer, alpha):
     for i in range(epochs):
         with tf.GradientTape() as tape:
             out = model(data)
-            #this is the unsupervised loss
+            # this is the unsupervised loss
             loss_unsup = tf.reduce_sum(model.losses)
 
-            #Here is the supervised loss calculation as specified in the paper
-            out_pos1 = model(pos_pairs[:,0])[0] #we only need H here, not B, which is why we take the 0 index at the end
-            out_pos2 = model(pos_pairs[:,1])[0]
-            out_neg1 = model(neg_pairs[:,0])[0]
-            out_neg2 = model(neg_pairs[:,1])[0]
-            pos_diff = out_pos1-out_pos2
+            # Here is the supervised loss calculation as specified in the paper
+            out_pos1 = model(pos_pairs[:, 0])[
+                0]  # we only need H here, not B, which is why we take the 0 index at the end
+            out_pos2 = model(pos_pairs[:, 1])[0]
+            out_neg1 = model(neg_pairs[:, 0])[0]
+            out_neg2 = model(neg_pairs[:, 1])[0]
+            pos_diff = out_pos1 - out_pos2
             neg_diff = out_neg1 - out_neg2
-            N_pos = tf.cast(tf.shape(pos_pairs)[0],tf.float32)
-            N_neg = tf.cast(tf.shape(neg_pairs)[0],tf.float32)
-            sigma_W = 1/N_pos * tf.matmul(tf.transpose(pos_diff),pos_diff)
+            N_pos = tf.cast(tf.shape(pos_pairs)[0], tf.float32)
+            N_neg = tf.cast(tf.shape(neg_pairs)[0], tf.float32)
+            sigma_W = 1 / N_pos * tf.matmul(tf.transpose(pos_diff), pos_diff)
             sigma_B = 1 / N_neg * tf.matmul(tf.transpose(neg_diff), neg_diff)
-            loss_sup = loss_unsup + alpha * tf.linalg.trace(sigma_W-sigma_B)
+            loss_sup = loss_unsup + alpha * tf.linalg.trace(sigma_W - sigma_B)
             loss = loss_unsup + loss_sup
 
-            #taking the gradient and applying the optimizer
-            grad = tape.gradient(loss,model.trainable_variables)
-            optimizer.apply_gradients(zip(grad,model.trainable_variables))
+            # taking the gradient and applying the optimizer
+            grad = tape.gradient(loss, model.trainable_variables)
+            optimizer.apply_gradients(zip(grad, model.trainable_variables))
             print(loss)
-
 
 # d = 100
 # N = 1000
@@ -123,6 +123,3 @@ def train_supervised(model, epochs, pos_pairs, neg_pairs, data, optimizer, alpha
 # train_unsupervised(model,50,sample_data,opt)
 # pos_samples, neg_samples = np.random.random(size=[N,2,d]), np.random.random(size=[N,2,d])
 # train_supervised(model,10,pos_samples,neg_samples,sample_data,opt,.01)
-
-
-
